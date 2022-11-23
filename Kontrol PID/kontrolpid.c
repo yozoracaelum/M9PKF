66a6431b423f49889cae025798848a94fb0e0928
Julian Evan <86104516+yozoracaelum@users.noreply.github.com>
2022-11-24 00:57:50 +0700

    Add files via upload

---
+++ Kontrol PID/kontrolpid.c
@@ -0,0 +1,245 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.14 Advanced
+Automatic Program Generator
+© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 11/24/2022
+Author  : 
+Company : 
+Comments: 
+
+
+Chip type               : ATmega8535
+Program type            : Application
+AVR Core Clock frequency: 11.059200 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 128
+*******************************************************/
+
+#include <mega8535.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <io.h> 
+#include <math.h>
+#include <delay.h>
+
+// Alphanumeric LCD functions
+#include <alcd.h>
+
+// Declare your global variables here
+float KP=1, KI=0.5, KD=0.0005, error_sblmI=0,ntc,settingpoin=33, error_sblmD=0, outPID, Tc=0.01, error, errorI,errorD, outP, outI, outD,hasil,hasil1,result,nilai_sensor;
+int x;
+unsigned char temp[30], kirim[30];
+// Standard Input/Output functions
+#include <stdio.h>
+
+// Voltage Reference: AREF pin
+#define ADC_VREF_TYPE ((0<<REFS1) | (0<<REFS0) | (0<<ADLAR))
+
+// Read the AD conversion result
+unsigned int read_adc(unsigned char adc_input)
+{
+ADMUX=adc_input | ADC_VREF_TYPE;
+// Delay needed for the stabilization of the ADC input voltage
+delay_us(10);
+// Start the AD conversion
+ADCSRA|=(1<<ADSC);
+// Wait for the AD conversion to complete
+while ((ADCSRA & (1<<ADIF))==0);
+ADCSRA|=(1<<ADIF);
+return ADCW;
+}
+void set_pwm(int fase){
+unsigned int buff_fase = (unsigned int)((40.15*(float)fase));
+OCR1AH = (unsigned char)(((buff_fase & (unsigned int)0xFF00))>>8); 
+OCR1AL = (unsigned char) (buff_fase & (unsigned int)0x00FF); }
+void main(void)
+{
+// Declare your local variables here
+
+// Input/Output Ports initialization
+// Port A initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (0<<DDA3) | (0<<DDA2) | (0<<DDA1) | (0<<DDA0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+
+// Port B initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (0<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+
+// Port C initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRC=(0<<DDC7) | (0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (0<<DDC0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+// Port D initialization
+// Function: Bit7=In Bit6=In Bit5=Out Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRD=(0<<DDD7) | (0<<DDD6) | (1<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+// State: Bit7=T Bit6=T Bit5=0 Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: Timer 0 Stopped
+// Mode: Normal top=0xFF
+// OC0 output: Disconnected
+TCCR0=(0<<WGM00) | (0<<COM01) | (0<<COM00) | (0<<WGM01) | (0<<CS02) | (0<<CS01) | (0<<CS00);
+TCNT0=0x00;
+OCR0=0x00;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: 10.800 kHz
+// Mode: Fast PWM top=0x03FF
+// OC1A output: Inverted PWM
+// OC1B output: Disconnected
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer Period: 94.815 ms
+// Output Pulse(s):
+// OC1A Period: 94.815 ms Width: 94.815 ms
+// Timer1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=(1<<COM1A1) | (1<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (1<<WGM11) | (1<<WGM10);
+TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (1<<WGM12) | (1<<CS12) | (0<<CS11) | (1<<CS10);
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: Timer2 Stopped
+// Mode: Normal top=0xFF
+// OC2 output: Disconnected
+ASSR=0<<AS2;
+TCCR2=(0<<WGM20) | (0<<COM21) | (0<<COM20) | (0<<WGM21) | (0<<CS22) | (0<<CS21) | (0<<CS20);
+TCNT2=0x00;
+OCR2=0x00;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (0<<OCIE0) | (0<<TOIE0);
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: Off
+// INT2: Off
+MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
+MCUCSR=(0<<ISC2);
+
+// USART initialization
+// Communication Parameters: 8 Data, 1 Stop, No Parity
+// USART Receiver: Off
+// USART Transmitter: On
+// USART Mode: Asynchronous
+// USART Baud Rate: 9600
+UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
+UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
+UBRRH=0x00;
+UBRRL=0x47;
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// The Analog Comparator's positive input is
+// connected to the AIN0 pin
+// The Analog Comparator's negative input is
+// connected to the AIN1 pin
+ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
+
+// ADC initialization
+// ADC Clock frequency: 691.200 kHz
+// ADC Voltage Reference: AREF pin
+// ADC High Speed Mode: Off
+// ADC Auto Trigger Source: ADC Stopped
+ADMUX=ADC_VREF_TYPE;
+ADCSRA=(1<<ADEN) | (0<<ADSC) | (0<<ADATE) | (0<<ADIF) | (0<<ADIE) | (1<<ADPS2) | (0<<ADPS1) | (0<<ADPS0);
+SFIOR=(1<<ADHSM) | (0<<ADTS2) | (0<<ADTS1) | (0<<ADTS0);
+
+// SPI initialization
+// SPI disabled
+SPCR=(0<<SPIE) | (0<<SPE) | (0<<DORD) | (0<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<SPR1) | (0<<SPR0);
+
+// TWI initialization
+// TWI disabled
+TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
+
+// Alphanumeric LCD initialization
+// Connections are specified in the
+// Project|Configure|C Compiler|Libraries|Alphanumeric LCD menu:
+// RS - PORTC Bit 7
+// RD - PORTC Bit 6
+// EN - PORTC Bit 5
+// D4 - PORTC Bit 4
+// D5 - PORTC Bit 3
+// D6 - PORTC Bit 2
+// D7 - PORTC Bit 1
+// Characters/line: 16
+lcd_init(16);
+
+while (1)
+      {
+      // Place your code here
+        ntc = read_adc(0);
+        settingpoin = read_adc(1)*0.097;
+        hasil= log(10000.0*(ntc/(1023.0-ntc))) ;
+        hasil1 =1/(((0.000017+(0.00037*hasil))-(0.00000031*hasil*hasil*hasil))); 
+        nilai_sensor =((float)hasil1-273.15);
+        error=settingpoin-nilai_sensor; 
+        if(error<0){
+            outPID=0; 
+            set_pwm(outPID);
+        } 
+        if(error>4){ 
+            outPID=255;
+            set_pwm(outPID);
+        }
+        else if (error<= 4&&error>=0){ 
+            outP=KP*error; //kendali integral 
+            errorI=error+error_sblmI; 
+            outI=KI*errorI*Tc; 
+            error_sblmI=errorI;//kendali Deferensial 
+            errorD=error-error_sblmD; 
+            outD=(KD*errorD)/Tc;
+            error_sblmD=errorD; //kontrol PID, merupakan gabungan dari kontrol P, I dan D 
+            outPID=outP+outI+outD;
+            set_pwm(outPID); 
+        } 
+        lcd_gotoxy(0,0); 
+        lcd_putsf("NTC"); 
+        lcd_gotoxy(6,0); 
+        lcd_putsf("="); 
+        lcd_gotoxy(8,0);
+        sprintf(temp,"%0.0f",nilai_sensor); 
+        lcd_puts(temp);
+        lcd_gotoxy(13,0); 
+        lcd_putchar(0xdf);//menampilkan karakter derajat 
+        lcd_putsf("C");
+        lcd_gotoxy(0,1); 
+        lcd_putsf("SP"); 
+        lcd_gotoxy(6,1); 
+        lcd_putsf("="); 
+        lcd_gotoxy(8,1);
+        sprintf(kirim,"%0.0f",settingpoin); 
+        lcd_puts(kirim); 
+        lcd_gotoxy(13,1);
+        lcd_putchar(0xdf);//menampilkan karakter derajat 
+        lcd_putsf("C"); 
+        printf("%0.1f,%0.0f\r\n",nilai_sensor,settingpoin);
+      }
+}
